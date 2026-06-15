6db568dba9935a002afee0d7506f95fad565ce48
Rossini-2109 <rossini2136@gmail.com>
2026-06-15 17:04:52 +0530

    Supabase changed2

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,12 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL!;
-
-const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-  console.log(
+console.log(
   "SUPABASE URL:",
   process.env.NEXT_PUBLIC_SUPABASE_URL
 );
@@ -15,7 +9,8 @@ console.log(
   "SUPABASE KEY EXISTS:",
   !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
+
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
