3826a2a4cf5335f023b719b0577590f36241f05a
Rossini-2109 <rossini2136@gmail.com>
2026-06-16 13:12:28 +0530

    Quiz Controller changes13

--- frontend/src/app/teacher/live-session/[sessionId]/page.tsx
+++ frontend/src/app/teacher/live-session/[sessionId]/page.tsx
@@ -1,14 +1,7 @@
 "use client";
 
 import TeacherLiveSessionPage from "@/components/TeacherLiveSessionPage";
-import { useParams } from "next/navigation";
 
 export default function Page() {
-  const params = useParams();
-
-  return (
-    <TeacherLiveSessionPage
-      sessionId={params.sessionId as string}
-    />
-  );
+  return <TeacherLiveSessionPage />;
 }
