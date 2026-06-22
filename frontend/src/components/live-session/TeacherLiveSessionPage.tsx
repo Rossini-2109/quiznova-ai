3952c022e09e239a47d7fe9f0e62f407987415cc
Rossini-2109 <rossini2136@gmail.com>
2026-06-22 10:51:04 +0530

    LiveQuiz file2

--- frontend/src/components/live-session/TeacherLiveSessionPage.tsx
+++ frontend/src/components/live-session/TeacherLiveSessionPage.tsx
@@ -1,20 +1,43 @@
-return (
-  <>
-    <Header />
+"use client";
 
-    {!session.isStarted ? (
-      <LobbyView
-        session={session}
-        participants={filteredParticipants}
-        onRemove={handleRemoveParticipant}
-      />
-    ) : (
-      <LiveDashboard
-        participants={filteredParticipants}
-        questions={questions}
-        analysis={questionAnalysis}
-        onRemove={handleRemoveParticipant}
-      />
-    )}
-  </>
-);
+import React from "react";
+import Header from "@/components/Header";
+import LobbyView from "./LobbyView";
+import LiveDashboard from "./LiveDashboard";
+
+interface Props {
+  session: any;
+  filteredParticipants: any[];
+  questions: any[];
+  questionAnalysis: any;
+  handleRemoveParticipant: (id: string) => void;
+}
+
+export default function TeacherLiveSessionPage({
+  session,
+  filteredParticipants,
+  questions,
+  questionAnalysis,
+  handleRemoveParticipant,
+}: Props) {
+  return (
+    <>
+      <Header />
+
+      {!session?.isStarted ? (
+        <LobbyView
+          session={session}
+          participants={filteredParticipants}
+          onRemove={handleRemoveParticipant}
+        />
+      ) : (
+        <LiveDashboard
+          participants={filteredParticipants}
+          questions={questions}
+          analysis={questionAnalysis}
+          onRemove={handleRemoveParticipant}
+        />
+      )}
+    </>
+  );
+}
