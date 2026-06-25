6b0258d13dc4ba8d9eb756020aefb2ba18cefe68
Rossini-2109 <rossini2136@gmail.com>
2026-06-25 09:38:44 +0530

    Final review changes9

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  experimental: {
-    turbopack: {
-      root: ".",
-    },
-  },
+
   images: {
     remotePatterns: [
       {
